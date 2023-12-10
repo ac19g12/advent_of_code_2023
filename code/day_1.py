34b672b566bfccae91af3a6304923e605dd4a14e
Andrew Clarke <werdna13@googlemail.com>
2023-12-10 13:02:33 +0000

    formating and update to util

--- code/day_1.py
+++ code/day_1.py
@@ -2,55 +2,60 @@ from util import Data
 
 Day_1 = Data(1)
 
-#%% Part A
-def part_a(text_input:list)->int:
+
+# %% Part A
+def part_a(text_input: list) -> int:
     result = 0
     for line in text_input:
         numbers = [letter for letter in line if letter.isnumeric()]
-        text_value = numbers[0]+numbers[-1]
+        text_value = numbers[0] + numbers[-1]
         result += int(text_value)
     return result
 
-#%% Part B
+
+# %% Part B
 NUMBERS = {
-           "one":"1",
-           "two":"2",
-           "three":"3",
-           "four":"4",
-           "five":"5",
-           "six":"6",
-           "seven":"7",
-           "eight":"8",
-           "nine":"9"
-        }
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+}
 
-def replace_str_index(text,index=0,replacement=''):
-    return f'{text[:index]}{replacement}{text[index+1:]}'
 
-def part_b(test_input:list)->int:
+def replace_str_index(text, index=0, replacement=""):
+    return f"{text[:index]}{replacement}{text[index+1:]}"
+
+
+def part_b(test_input: list) -> int:
     new_text = []
     for line in test_input:
         pos = [line.find(key) for key in NUMBERS]
-        pos_filter = [_p for _p in pos if _p>=0]
+        pos_filter = [_p for _p in pos if _p >= 0]
         if pos_filter:
             _min_p = min(pos_filter)
-            min_value = [_v for _v, _p in zip(NUMBERS,pos) if _p==_min_p][0]
+            min_value = [_v for _v, _p in zip(NUMBERS, pos) if _p == _min_p][0]
             line = replace_str_index(line, _min_p, NUMBERS[min_value])
-    
+
         pos = [line.rfind(key) for key in NUMBERS]
-        pos_filter = [_p for _p in pos if _p>=0]
+        pos_filter = [_p for _p in pos if _p >= 0]
         if pos_filter:
             _max_p = max(pos_filter)
-            max_value = [_v for _v, _p in zip(NUMBERS,pos) if _p==_max_p][0]
+            max_value = [_v for _v, _p in zip(NUMBERS, pos) if _p == _max_p][0]
 
             line = replace_str_index(line, _max_p, NUMBERS[max_value])
         new_text.append(line)
     return part_a(new_text)
 
-#%% Running Code
+
+# %% Running Code
 if __name__ == "__main__":
-    score_a = part_a(Day_1.INPUT_1.to_list())
+    score_a = part_a(Day_1.INPUT_1.to_list()) # type: ignore
     print(f"day 1 part a: {score_a}")
 
-    score_b = part_b(Day_1.INPUT_1.to_list())
+    score_b = part_b(Day_1.INPUT_1.to_list()) # type: ignore
     print(f"day 1 part b: {score_b}")
--- code/util.py
+++ code/util.py
@@ -1,53 +1,74 @@
 import os
 import yaml
+from importlib.machinery import SourceFileLoader
+
 
 class DataStr(str):
     def test(self):
         print(self)
 
-    def to_list(self):
-        return self.split(" ")
+    def to_list(self, new_line: str = " "):
+        return self.split(new_line)
 
-class Data():
-    def __init__(self,
-                 day:int,
-                 data_folder_name:str="_data",
-                 data_prefrix:str="day_",
-                 data_file_type:str="yaml"):
 
-        base_path = os.path.dirname(os.getcwd())
+def find_modules(module: object) -> dict:
+    names = [_name for _name in dir(module) if not ("__" in _name)]
+    result_dict = {}
+    for _var in names:
+        result_dict[_var] = getattr(module, _var)
+    return result_dict
+
+
+class Data:
+    def __init__(
+        self,
+        day: int,
+        data_folder_name: str = "_data",
+        data_prefrix: str = "day_",
+        data_file_type: str = "yaml",
+    ):
+        base_path = os.getcwd()
         data_path = os.path.join(base_path, data_folder_name)
 
-        file_name = f"{data_prefrix}{day}.{data_file_type}"
+        file_nickname = f"{data_prefrix}{day}"
+        file_name = f"{file_nickname}.{data_file_type}"
         file_path = os.path.join(data_path, file_name)
 
-        with open(file_path, 'r') as file:
-            if data_file_type == "yaml":
+        if data_file_type == "yaml":
+            with open(file_path, "r") as file:
                 data = yaml.safe_load(file)
-            else:
-                raise ValueError("No other data types allowed, only YAML")
+        elif data_file_type == "py":
+            loader = SourceFileLoader(file_nickname, file_path)
+            handle = loader.load_module(file_nickname)
+            data = find_modules(handle)
+        else:
+            raise ValueError("No other data types allowed, only YAML")
 
         for key, value in data.items():
             self.__dict__[key] = DataStr(value.strip())
 
     @staticmethod
-    def add_file(data:dict, 
-                 day:int,
-                 data_folder_name:str="_data",
-                 data_prefrix:str="day_",
-                 data_file_type:str="yaml") -> None:
-        
+    def add_file(
+        data: dict,
+        day: int,
+        data_folder_name: str = "_data",
+        data_prefrix: str = "day_",
+        data_file_type: str = "yaml",
+    ) -> None:
         base_path = os.path.dirname(os.getcwd())
         data_path = os.path.join(base_path, data_folder_name)
 
         file_name = f"{data_prefrix}{day}.{data_file_type}"
         file_path = os.path.join(data_path, file_name)
 
-        with open(file_path, 'w') as file:
+        with open(file_path, "w") as file:
             if data_file_type == "yaml":
-                data = yaml.dump(data, file, default_flow_style=False)
+                yaml.dump(data, file, default_flow_style=False)
             else:
                 raise ValueError("No other data types allowed, only YAML")
 
         return None
-    
+
+
+if __name__ == "__main__":
+    Data(2, data_file_type="py")
